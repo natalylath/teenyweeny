8f45da003cb23daec8b9db908a8cf6a332447369
natalylath <nataly.lath@gmail.com>
2015-03-31 18:48:22 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,58 +1,42 @@
 $(document).ready(function() {
+	
+	/* common scripts */
 	$('ul.tabs').on('click', 'li:not(.selected)', function() {  
 	    $(this).addClass('selected').siblings().removeClass('selected')  
 	    .parents('div.tabs-cont').find('div.tab-box').eq($(this).index()).fadeIn(150).siblings('div.tab-box').hide();
 	});
 
-	function priceList() {
+	$('.select2').select2({
+		minimumResultsForSearch: 20
+	});
 
+	function priceList() {
 		function format(item) {
 		    var originalOption = $(item.element);
-		    return '<span class="pr">' + item.text + '</span>' + originalOption.data('volume');
+		    return '<span class="pr">' + item.text + '</span>' + '<span class="val">' + originalOption.data('volume') + '</span>';
 		};
- 
-		$('.select2').select2({
-			'width': 180,
+		$('.select2-complex').select2({
+			'width': 135,
 			formatResult: format,
 			formatSelection: format,
 			minimumResultsForSearch: 20
 		});
-
 	};
-	
 	priceList();
 
-	if ($('.bxslider').length) {
-		$('.bxslider').bxSlider({
-			pager: false,
-			nextSelector: '.bx-arrow-right',
-			prevSelector: '.bx-arrow-left',
-			nextText: '',
-			prevText: '',
-			slideWidth: 106,
-			minSlides: 6,
-			maxSlides: 6
-		});
-	};
+    $('.fancybox').fancybox({
+    });
 
-	function filterBlock() {
-		
-		$(document).on('click', '.filter-title', function() {
-			var par = $(this).closest('.filter-block');
-			par.toggleClass('opened');
-			if (par.hasClass('opened')) {
-				par.find('.filter-content').slideDown();
-			} else {
-				par.find('.filter-content').slideUp();
-			};
-		});
-
-	};
-	filterBlock();
+    $(document).on('click', '.top-page', function(e) {
+    	$.scrollTo(0, 500);
+    	e.preventDefault();
+    });
 
 
-    $( ".slider-range" ).each(function() {
-        $( ".slider-range" ).slider({
+
+    /* catalog scripts */
+    $(".slider-range").each(function() {
+        $(".slider-range").slider({
           range: true,
           min: 300,
           max: 5900,
@@ -68,7 +52,46 @@ $(document).ready(function() {
 
         $(this).find('.ui-slider-handle').eq(0).addClass('left-handle');  
     });
-    
+
+    function filterBlock() {
+		$(document).on('click', '.filter-title', function() {
+			var par = $(this).closest('.filter-block');
+			par.toggleClass('opened');
+			if (par.hasClass('opened')) {
+				par.find('.filter-content').slideDown();
+			} else {
+				par.find('.filter-content').slideUp();
+			};
+		});
+	};
+	filterBlock();
+
+	if ($('.bxslider').length) {
+		$('.bxslider').bxSlider({
+			pager: false,
+			nextSelector: '.bx-arrow-right',
+			prevSelector: '.bx-arrow-left',
+			nextText: '',
+			prevText: '',
+			slideWidth: 106,
+			minSlides: 6,
+			maxSlides: 6
+		});
+	};
+
+	$(document).on('click', '.sort-btn', function(e) {
+		if ($(this).hasClass('active')) {
+			if ($(this).hasClass('sort-down')) {
+				$(this).removeClass('sort-down').addClass('sort-up');
+			} else {
+				$(this).removeClass('sort-up').addClass('sort-down');
+			}
+		} else {
+			$(this).closest('.sort').find('.sort-btn').removeClass('active').removeClass('sort-up').removeClass('sort-down');
+			$(this).addClass('active').addClass('sort-down');
+		};
+		e.preventDefault();
+	});
 
 });
 
