d58bdd8594a57499f62aa5afdcf9213c873ceb6d
natalylath <nataly.lath@gmail.com>
2015-04-01 16:48:54 +0300

    update

--- js/script.js
+++ js/script.js
@@ -33,8 +33,22 @@ $(document).ready(function() {
     });
 
 
-
     /* catalog scripts */
+    $(document).on('click', '.brand-btn-cont .btn', function(e) {
+    	$(this).toggleClass('opened');
+    	if ($(this).hasClass('opened')) {
+    		$(this).closest('.brand-more').find('.catalog-list').slideDown();
+    		$(this).text('Свернуть бренды');
+    	} else {
+    		$(this).closest('.brand-more').find('.catalog-list').slideUp();
+    		$(this).text($(this).data('originaltxt'));
+    	}
+    	
+    	e.preventDefault();
+    });
+
+
+    /* catalog filter scripts */
     $(".slider-range").each(function() {
         $(".slider-range").slider({
           range: true,
@@ -91,6 +105,55 @@ $(document).ready(function() {
 			$(this).addClass('active').addClass('sort-down');
 		};
 		e.preventDefault();
+	});
+
+	$(document).on('click', '.popup-btn', function(e) {
+		$(this).toggleClass('opened');
+		var layer = $(this).closest('.popup-cont').find('.popup-layer');
+		if ($(this).hasClass('opened')) {
+			if ($(this).data('yoffset') != '' && $(this).data('yoffset')) {
+				layer.css({'top': $(this).data('yoffset') + 'px'});
+			};
+			layer.show();
+		} else {
+			layer.hide();
+		};
+		e.preventDefault();
+	});
+	$(document).on('click', '.popup-close', function() {
+		$(this).closest('.popup-layer').hide();
+		$(this).closest('.popup-cont').find('.popup-btn').removeClass('opened');
+	});
+
+	$(document).on('focusin', '.tag-input', function() {
+		$(this).closest('.autocomplete-cont').find('.autocomplete').show();
+	});
+	$(document).on('focuout', '.tag-input', function() {
+		$(this).closest('.autocomplete-cont').find('.autocomplete').hide();
+	});
+
+	$(document).on('click', '.tag-remove', function() {
+		$(this).closest('.tag').hide();
+	});
+	$(document).on('click', '.autocomplete-line', function() {
+		$(this).closest('.popup-layer').find('.tags').html('<div class="tag">' + $(this).text() + '<span class="tag-remove"></span></div>');
+		$(this).closest('.autocomplete').hide();
+		$(this).closest('.autocomplete-cont').find('.tag-input').val($(this).text());
+	});
+
+	/* add ajax for loading content of specified breed! */
+	$(document).on('click','.autocomplete-submit', function(e) {
+		$(this).closest('.popup-layer').hide();
+		$(this).closest('.popup-cont').find('.popup-btn').removeClass('opened');
+		$('.breed-btn').addClass('active').text($(this).closest('.popup-cont').find('.tag-input').val());
+		e.preventDefault();
+	});
+
+	$(document).on('mouseenter', '.breed-btn', function() {
+		$(this).closest('.breed-cont').find('.tip-layer').show();
+	});
+	$(document).on('mouseleave', '.breed-btn', function() {
+		$(this).closest('.breed-cont').find('.tip-layer').hide();
 	});
 
 });
