a6ec20d6ed2c787ddadd57625eb86f8d3e86ad9b
l.sokolova <l.sokolova@rambler-co.ru>
2015-04-02 18:02:51 +0300

    gulp

---
+++ build/static/js/script.js
@@ -0,0 +1,159 @@
+$(document).ready(function() {
+	
+	/* common scripts */
+	$('ul.tabs').on('click', 'li:not(.selected)', function() {  
+	    $(this).addClass('selected').siblings().removeClass('selected')  
+	    .parents('div.tabs-cont').find('div.tab-box').eq($(this).index()).fadeIn(150).siblings('div.tab-box').hide();
+	});
+
+	$('.select2').select2({
+		minimumResultsForSearch: 20
+	});
+
+	function priceList() {
+		function format(item) {
+		    var originalOption = $(item.element);
+		    return '<span class="pr">' + item.text + '</span>' + '<span class="val">' + originalOption.data('volume') + '</span>';
+		}
+		$('.select2-complex').select2({
+			'width': 135,
+			formatResult: format,
+			formatSelection: format,
+			minimumResultsForSearch: 20
+		});
+	}
+	priceList();
+
+    $('.fancybox').fancybox({
+    });
+
+    $(document).on('click', '.top-page', function(e) {
+    	$.scrollTo(0, 500);
+    	e.preventDefault();
+    });
+
+
+    /* catalog scripts */
+    $(document).on('click', '.brand-btn-cont .btn', function(e) {
+    	$(this).toggleClass('opened');
+    	if ($(this).hasClass('opened')) {
+    		$(this).closest('.brand-more').find('.catalog-list').slideDown();
+    		$(this).text('Свернуть бренды');
+    	} else {
+    		$(this).closest('.brand-more').find('.catalog-list').slideUp();
+    		$(this).text($(this).data('originaltxt'));
+    	}
+    	
+    	e.preventDefault();
+    });
+
+
+    /* catalog filter scripts */
+    $(".slider-range").each(function() {
+        $(".slider-range").slider({
+          range: true,
+          min: 300,
+          max: 5900,
+          values: [ 800, 2100 ],
+          slide: function( event, ui ) {
+            $(this).parents('.slider-form').find( ".min" ).val(ui.values[ 0 ]);
+            $(this).parents('.slider-form').find( ".max" ).val(ui.values[ 1 ]);
+          }
+        });
+    
+        $(this).parents('.slider-form').find( ".min" ).val( $( ".slider-range" ).slider( "values", 0 ));
+        $(this).parents('.slider-form').find( ".max" ).val( $( ".slider-range" ).slider( "values", 1 ));
+
+        $(this).find('.ui-slider-handle').eq(0).addClass('left-handle');  
+    });
+
+    function filterBlock() {
+		$(document).on('click', '.filter-title', function() {
+			var par = $(this).closest('.filter-block');
+			par.toggleClass('opened');
+			if (par.hasClass('opened')) {
+				par.find('.filter-content').slideDown();
+			} else {
+				par.find('.filter-content').slideUp();
+			};
+		});
+	};
+	filterBlock();
+
+	if ($('.bxslider').length) {
+		$('.bxslider').bxSlider({
+			pager: false,
+			nextSelector: '.bx-arrow-right',
+			prevSelector: '.bx-arrow-left',
+			nextText: '',
+			prevText: '',
+			slideWidth: 106,
+			minSlides: 6,
+			maxSlides: 6
+		});
+	};
+
+	$(document).on('click', '.sort-btn', function(e) {
+		if ($(this).hasClass('active')) {
+			if ($(this).hasClass('sort-down')) {
+				$(this).removeClass('sort-down').addClass('sort-up');
+			} else {
+				$(this).removeClass('sort-up').addClass('sort-down');
+			}
+		} else {
+			$(this).closest('.sort').find('.sort-btn').removeClass('active').removeClass('sort-up').removeClass('sort-down');
+			$(this).addClass('active').addClass('sort-down');
+		};
+		e.preventDefault();
+	});
+
+	$(document).on('click', '.popup-btn', function(e) {
+		$(this).toggleClass('opened');
+		var layer = $(this).closest('.popup-cont').find('.popup-layer');
+		if ($(this).hasClass('opened')) {
+			if ($(this).data('yoffset') != '' && $(this).data('yoffset')) {
+				layer.css({'top': $(this).data('yoffset') + 'px'});
+			};
+			layer.show();
+		} else {
+			layer.hide();
+		};
+		e.preventDefault();
+	});
+	$(document).on('click', '.popup-close', function() {
+		$(this).closest('.popup-layer').hide();
+		$(this).closest('.popup-cont').find('.popup-btn').removeClass('opened');
+	});
+
+	$(document).on('focusin', '.tag-input', function() {
+		$(this).closest('.autocomplete-cont').find('.autocomplete').show();
+	});
+	$(document).on('focuout', '.tag-input', function() {
+		$(this).closest('.autocomplete-cont').find('.autocomplete').hide();
+	});
+
+	$(document).on('click', '.tag-remove', function() {
+		$(this).closest('.tag').hide();
+	});
+	$(document).on('click', '.autocomplete-line', function() {
+		$(this).closest('.popup-layer').find('.tags').html('<div class="tag">' + $(this).text() + '<span class="tag-remove"></span></div>');
+		$(this).closest('.autocomplete').hide();
+		$(this).closest('.autocomplete-cont').find('.tag-input').val($(this).text());
+	});
+
+	/* add ajax for loading content of specified breed! */
+	$(document).on('click','.autocomplete-submit', function(e) {
+		$(this).closest('.popup-layer').hide();
+		$(this).closest('.popup-cont').find('.popup-btn').removeClass('opened');
+		$('.breed-btn').addClass('active').text($(this).closest('.popup-cont').find('.tag-input').val());
+		e.preventDefault();
+	});
+
+	$(document).on('mouseenter', '.breed-btn', function() {
+		$(this).closest('.breed-cont').find('.tip-layer').show();
+	});
+	$(document).on('mouseleave', '.breed-btn', function() {
+		$(this).closest('.breed-cont').find('.tip-layer').hide();
+	});
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    rigger = require('gulp-rigger'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: {
+        html: 'build',
+        fonts: 'build/static/fonts/',
+        css: 'build/static/css/',
+        js: 'build/static/js/',
+        img: 'build/static/images/'
+
+    },
+    src: {
+        html: 'src/templates/*.html',
+        fonts: 'src/static/fonts/**/*.*',
+        css: 'src/static/css/**/*.css',
+        scss: 'src/static/scss/**/style.scss',
+        js: 'src/static/js/**/*.js',
+        img: 'src/static/images/**/*.*'
+    },
+    watch: {
+        html: 'src/templates/**/*.html',
+        fonts: 'src/static/fonts/**/*.*',
+        css: 'src/static/css/**/*.css',
+        scss: 'src/static/css/**/*.scss',
+        js: 'src/static/js/**/*.js',
+        img: 'src/static/images/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('css:build', function () {
+    gulp.src(path.src.css)
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('scss:build', function () {
+    gulp.src(path.src.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('build', [
+    'html:build',
+    'fonts:build',
+    'css:build',
+    'scss:build',
+    'js:build',
+    'image:build'
+]);
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('css:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('scss:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
