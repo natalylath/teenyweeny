7bcf48f261556d91e23e4f30db80475ebd0322bb
natalylath <nataly.lath@gmail.com>
2015-04-03 19:16:01 +0300

    my html and other files but images paths - not correct

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	
+
 	/* common scripts */
-	$('ul.tabs').on('click', 'li:not(.selected)', function() {  
-	    $(this).addClass('selected').siblings().removeClass('selected')  
-	    .parents('div.tabs-cont').find('div.tab-box').eq($(this).index()).fadeIn(150).siblings('div.tab-box').hide();
+	$('ul.tabs').on('click', 'li:not(.selected)', function() {
+		$(this).addClass('selected').siblings().removeClass('selected')
+			.parents('div.tabs-cont').find('div.tab-box').eq($(this).index()).fadeIn(150).siblings('div.tab-box').hide();
 	});
 
 	$('.select2').select2({
@@ -12,63 +12,103 @@ $(document).ready(function() {
 
 	function priceList() {
 		function format(item) {
-		    var originalOption = $(item.element);
-		    return '<span class="pr">' + item.text + '</span>' + '<span class="val">' + originalOption.data('volume') + '</span>';
-		}
+			var originalOption = $(item.element);
+			return '<span class="pr">' + item.text + '</span>' + '<span class="val">' + originalOption.data('volume') + '</span>';
+		};
 		$('.select2-complex').select2({
 			'width': 135,
 			formatResult: format,
 			formatSelection: format,
 			minimumResultsForSearch: 20
 		});
-	}
+	};
 	priceList();
 
-    $('.fancybox').fancybox({
-    });
+	$('.fancybox').fancybox({
+		padding: 0,
+		fitToView: false,
+		tpl: {
+			closeBtn : '<a class="fancybox-item fancybox-close close2"></a>'
+		}
+	});
 
-    $(document).on('click', '.top-page', function(e) {
-    	$.scrollTo(0, 500);
-    	e.preventDefault();
-    });
-
-
-    /* catalog scripts */
-    $(document).on('click', '.brand-btn-cont .btn', function(e) {
-    	$(this).toggleClass('opened');
-    	if ($(this).hasClass('opened')) {
-    		$(this).closest('.brand-more').find('.catalog-list').slideDown();
-    		$(this).text('Свернуть бренды');
-    	} else {
-    		$(this).closest('.brand-more').find('.catalog-list').slideUp();
-    		$(this).text($(this).data('originaltxt'));
-    	}
-    	
-    	e.preventDefault();
-    });
+	$(document).on('click', '.top-page', function(e) {
+		$.scrollTo(0, 500);
+		e.preventDefault();
+	});
 
 
-    /* catalog filter scripts */
-    $(".slider-range").each(function() {
-        $(".slider-range").slider({
-          range: true,
-          min: 300,
-          max: 5900,
-          values: [ 800, 2100 ],
-          slide: function( event, ui ) {
-            $(this).parents('.slider-form').find( ".min" ).val(ui.values[ 0 ]);
-            $(this).parents('.slider-form').find( ".max" ).val(ui.values[ 1 ]);
-          }
-        });
-    
-        $(this).parents('.slider-form').find( ".min" ).val( $( ".slider-range" ).slider( "values", 0 ));
-        $(this).parents('.slider-form').find( ".max" ).val( $( ".slider-range" ).slider( "values", 1 ));
+	/* catalog scripts */
+	$(document).on('click', '.brand-btn-cont .btn', function(e) {
+		$(this).toggleClass('opened');
+		if ($(this).hasClass('opened')) {
+			$(this).closest('.brand-more').find('.catalog-list').slideDown();
+			$(this).text('Свернуть бренды');
+		} else {
+			$(this).closest('.brand-more').find('.catalog-list').slideUp();
+			$(this).text($(this).data('originaltxt'));
+		}
 
-        $(this).find('.ui-slider-handle').eq(0).addClass('left-handle');  
-    });
+		e.preventDefault();
+	});
 
-    function filterBlock() {
+
+	/* catalog filter scripts */
+	$(".slider-range").each(function() {
+		var par = $(this).parents('.slider-form');
+		var minVal =  $(".min", par);
+		var maxVal =  $(".max", par);
+		var currentSlider = $(this);
+		var MIN_PRICE = 300;
+		var MAX_PRICE = 20000;
+
+		$(".slider-range").slider({
+			range: true,
+			min: MIN_PRICE,
+			max: MAX_PRICE,
+			values: [ 800, 7000 ],
+			slide: function( event, ui ) {
+				minVal.val(ui.values[ 0 ]);
+				maxVal.val(ui.values[ 1 ]);
+			}
+		});
+
+		minVal.val( $( ".slider-range" ).slider( "values", 0 ));
+		maxVal.val( $( ".slider-range" ).slider( "values", 1 ));
+
+		minVal.change(function () {
+			var curValue = this.value;
+			var maxValue = maxVal.val();
+
+			if (parseInt(curValue) > maxVal.val()) {
+				curValue = MIN_PRICE;
+				maxValue = MAX_PRICE;
+			}
+			currentSlider.slider({values: [ parseInt(curValue), maxValue ]});
+		});
+
+		maxVal.change(function () {
+			var curValue = this.value;
+			var minValue = minVal.val();
+
+			if (parseInt(curValue) > MAX_PRICE) {
+				curValue = MAX_PRICE;
+			};
+			if (parseInt(curValue) < minValue) {
+				curValue = MAX_PRICE;
+				minValue = MIN_PRICE;
+			};
+
+			currentSlider.slider({values: [minValue, parseInt(curValue) ]});
+		});
+
+		$(this).find('.ui-slider-handle').eq(0).addClass('left-handle');
+	});
+
+	function filterBlock() {
 		$(document).on('click', '.filter-title', function() {
+			if ($(this).hasClass('special-title')) return;
+
 			var par = $(this).closest('.filter-block');
 			par.toggleClass('opened');
 			if (par.hasClass('opened')) {
@@ -77,6 +117,17 @@ $(document).ready(function() {
 				par.find('.filter-content').slideUp();
 			};
 		});
+
+		$(document).on('click', '.special-title', function() {
+			var par = $(this).closest('.filter-block');
+			par.toggleClass('opened');
+			if (par.hasClass('opened')) {
+				par.find('.subcontent').slideDown();
+			} else {
+				par.find('.subcontent').slideUp();
+			};
+		});
+
 	};
 	filterBlock();
 
@@ -133,7 +184,12 @@ $(document).ready(function() {
 	});
 
 	$(document).on('click', '.tag-remove', function() {
-		$(this).closest('.tag').hide();
+		if ($(this).closest('.tags').hasClass('toggle-tags')) {
+			$(this).closest('.tag').toggleClass('passive');
+		} else {
+			(this).closest('.tag').hide();
+		};
+
 	});
 	$(document).on('click', '.autocomplete-line', function() {
 		$(this).closest('.popup-layer').find('.tags').html('<div class="tag">' + $(this).text() + '<span class="tag-remove"></span></div>');
@@ -143,10 +199,12 @@ $(document).ready(function() {
 
 	/* add ajax for loading content of specified breed! */
 	$(document).on('click','.autocomplete-submit', function(e) {
-		$(this).closest('.popup-layer').hide();
-		$(this).closest('.popup-cont').find('.popup-btn').removeClass('opened');
-		$('.breed-btn').addClass('active').text($(this).closest('.popup-cont').find('.tag-input').val());
-		e.preventDefault();
+		if (!$(this).hasClass('link')) {
+			$(this).closest('.popup-layer').hide();
+			$(this).closest('.popup-cont').find('.popup-btn').removeClass('opened');
+			$('.breed-btn').addClass('active').text($(this).closest('.popup-cont').find('.tag-input').val());
+			e.preventDefault();
+		};
 	});
 
 	$(document).on('mouseenter', '.breed-btn', function() {
@@ -156,9 +214,50 @@ $(document).ready(function() {
 		$(this).closest('.breed-cont').find('.tip-layer').hide();
 	});
 
+	$(document).on('click', '.filter-option', function() {
+		$(this).addClass('active');
+	});
+	$(document).on('click', '.option-del', function(e) {
+		$(this).closest('.filter-option').removeClass('active');
+		e.stopImmediatePropagation();
+	});
+
+	/* card scripts */
+	$('.bxslider-photo').bxSlider({
+		pagerCustom: '#bxpager',
+		minSlides: 1,
+		maxSlides: 1,
+		slideWidth: 378,
+		nextText: '',
+		prevText: '',
+		nextSelector: '.bxslider-photo-border .bx-arrow-right',
+		prevSelector: '.bxslider-photo-border .bx-arrow-left',
+		onSlideAfter: function ($slideElement, oldIndex, newIndex) {
+			$('.bxpager').removeClass('active');
+			var el = $('.bxpager'+'.'+'b'+ newIndex).not('.bx-clone');
+			el.addClass('active');
+		}
+	});
+
+	$('#bxpager').bxSlider({
+		minSlides: 4,
+		maxSlides: 4,
+		moveSlides: 1,
+		slideWidth: 70,
+		pager: false,
+		slideMargin: 5,
+		nextText: '',
+		prevText: '',
+		nextSelector: '.bxpager-cont .bx-arrow-right',
+		prevSelector: '.bxpager-cont .bx-arrow-left',
+		onSliderLoad: function (currentIndex) {
+			$('#bxpager .bxpager').eq(0).addClass('active');
+		}
+	});
+	$(document).on('click','.bxpager', function() {
+		$('.bxpager').removeClass('active');
+		$(this).addClass('active');
+	});
+
+
 });
-
-
-
-
-
