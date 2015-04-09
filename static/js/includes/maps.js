4eaeec33ab8ce941e2059b90bc25af0763061f76
l.sokolova <l.sokolova@rambler-co.ru>
2015-04-09 17:45:37 +0300

    Доставка в москве

--- static/js/includes/maps.js
+++ static/js/includes/maps.js
@@ -1,16 +1,60 @@
-ymaps.ready(init);
-var myMap,
-    myPlacemark;
+var maps = (function(){
+    var nodes = {},
+        methods = {
+            prepare: function(){
+                if($('#map_moscow').length != 0) {
+                    methods.create.mapMoscow();
+                }
+                if($('#map_region').length != 0) {
+                    methods.create.mapRegion();
+                }
+            },
+            create: {
+                mapMoscow: function(){
+                    var map = new ymaps.Map("map_moscow", {
+                        center: [55.758497, 37.660291],
+                        zoom: 15,
+                        controls: []
+                    });
 
-function init(){
-    myMap = new ymaps.Map("map_moscow", {
-        center: [55.76, 37.64],
-        zoom: 15
-    });
+                    var placemark = new ymaps.Placemark([55.758497, 37.660291], {},{
+                        preset: 'islands#dotIcon'
+                    });
 
-    myPlacemark = new ymaps.Placemark([55.76, 37.64], {
-        balloonContent: 'Столица России'
-    });
+                    map.geoObjects.add(placemark);
+                },
+                mapRegion: function(){
+                    var map = new ymaps.Map("map_region", {
+                        center: [55.753676, 37.619899],
+                        zoom: 10,
+                        controls: ['zoomControl']
+                    },{
+                        minZoom: 9,
+                        maxZoom: 10
+                    });
 
-    myMap.geoObjects.add(myPlacemark);
-}
+                    var tileUrlTemplate = 'static/images/map/%z/x=%x&y=%y',
+                        keyTemplate = 'callback_tile_%c',
+                        imgUrlTemplate = 'static/images/map/%z/x=%x&y=%y.png',
+                        objSource = new ymaps.hotspot.ObjectSource(tileUrlTemplate, keyTemplate),
+                        imgLayer = new ymaps.Layer(imgUrlTemplate, {tileTransparent: true}),
+                        hotspotLayer = new ymaps.hotspot.Layer(objSource, {cursor: 'help'});
+
+                    map.layers.add(hotspotLayer);
+                    map.layers.add(imgLayer);
+                }
+            },
+            events: {
+                set: function(){
+                    nodes.body.on('click', '.s-scroll', methods.scrollTo);
+                }
+            }
+        };
+    return {
+        init: function(){
+            ymaps.ready(methods.prepare);
+        }
+    }
+}());
+
+maps.init();
