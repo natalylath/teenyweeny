1046d3a5754947cd27caa19af7be886f8d533be3
siauk <452100170sa>
2015-04-12 18:38:50 +0300

    js формы оформления заказа

--- static/js/includes/form.js
+++ static/js/includes/form.js
@@ -1,5 +1,8 @@
 var form = (function(){
-    var nodes = {
+    var vars= {
+            speed: 150
+        },
+        nodes = {
             body: $('body')
         },
         methods = {
@@ -14,6 +17,66 @@ var form = (function(){
                     .removeClass('btn_grey')
                     .addClass('btn_block');
             },
+            delivery: {
+                type: function(){
+                    var item = $(this);
+                    if(!item.hasClass('current')) {
+                        nodes.body.find('.form_substrate.current')
+                            .removeClass('current')
+                            .removeClass('corner');
+
+                        nodes.deliveryTypeForm.hide();
+
+                        item.addClass('current');
+
+                        if(item.data('form')) {
+                            item.addClass('corner');
+                            nodes.deliveryTypeForm.fadeIn(vars.speed);
+                        }
+                    }
+                },
+                address: function(){
+                    var item = $(this),
+                        parent = item.parent();
+
+                    nodes.body.find('.s-delivery-address-edit').show();
+                    nodes.body.find('.s-delivery-address-str').show();
+
+                    if(item.data('new')) {
+                        $.each(nodes.deliveryAddressForm.find('input[type="text"]'), function(){
+                            $(this).val('');
+                        });
+
+                        nodes.deliveryAddressForm
+                            .insertAfter(parent)
+                            .fadeIn(vars.speed);
+                    } else {
+                        nodes.deliveryAddressForm.hide();
+                    }
+
+                },
+                addressEdit: function(e){
+                    var item = $(this),
+                        parent = item.parent();
+
+                    nodes.body.find('.s-delivery-address-edit').show();
+                    nodes.body.find('.s-delivery-address-str').show();
+
+                    item.hide();
+                    parent.find('.s-delivery-address-str').hide();
+
+                    nodes.deliveryAddressFormStreet.val(parent.find('.street').text());
+                    nodes.deliveryAddressFormHouse.val(parent.find('.house').text());
+                    nodes.deliveryAddressFormCorp.val(parent.find('.corp').text());
+                    nodes.deliveryAddressFormOffice.val(parent.find('.office').text());
+
+                    nodes.deliveryAddressForm
+                        .insertAfter(parent)
+                        .fadeIn(vars.speed);
+
+                    e.preventDefault();
+                }
+            },
             depend: function(){
                 var item = $(this);
                 nodes.body.find('[data-dependent="' + item.data('depend') + '"]').attr('disabled', item.is(':checked'));
@@ -26,6 +89,9 @@ var form = (function(){
                     nodes.body
                         .on('change', '.s-change', methods.active)
                         .on('keyup', '.s-change', methods.active)
+                        .on('click', '.s-delivery-type', methods.delivery.type)
+                        .on('click', '.s-delivery-address', methods.delivery.address)
+                        .on('click', '.s-delivery-address-edit', methods.delivery.addressEdit)
                         .on('click', '.s-depend', methods.depend)
                         .on('change', '[type="file"]', methods.addFile);
 
@@ -34,6 +100,17 @@ var form = (function(){
         };
     return {
         init: function(){
+            nodes.deliveryTypeForm = nodes.body.find('.s-delivery-type-form');
+
+            nodes.deliveryAddressForm = nodes.body.find('.s-delivery-address-form');
+
+            if(nodes.deliveryAddressForm.length != 0) {
+                nodes.deliveryAddressFormStreet = nodes.deliveryAddressForm.find('#street');
+                nodes.deliveryAddressFormHouse = nodes.deliveryAddressForm.find('#house');
+                nodes.deliveryAddressFormCorp = nodes.deliveryAddressForm.find('#corp');
+                nodes.deliveryAddressFormOffice = nodes.deliveryAddressForm.find('#office');
+            }
+
             nodes.fileName = nodes.body.find('.form_file_name');
 
             methods.events.set();
