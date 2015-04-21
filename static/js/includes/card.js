a51ae910780f8367542eeaf2887f616776d6e39c
siauk <452100170sa>
2015-04-22 00:40:41 +0300

    прикрутила js пересчета суммы ко всем страницам + новый баннер

--- static/js/includes/card.js
+++ static/js/includes/card.js
@@ -4,59 +4,6 @@ var card = (function(){
             body: $('body')
         },
         methods = {
-            price: {
-                prepare: function(){
-                    nodes.countInput = nodes.body.find('.card-count-input');
-                    nodes.totalValue = nodes.body.find('.total-value');
-                    nodes.counterValue = nodes.body.find('.counter-value');
-                    nodes.cardTotal = nodes.body.find('.card-total');
-                },
-                increment: function(){
-                    var current = parseInt(nodes.countInput.val());
-
-                    nodes.countInput.val(current + 1);
-
-                    methods.price.updateTotalPrice();
-                },
-                decrement: function(){
-                    var current = parseInt(nodes.countInput.val());
-
-                    if (current < 2) return;
-                    nodes.countInput.val(current - 1);
-
-                    methods.price.updateTotalPrice();
-                },
-                change: function(){
-                    var current  = parseInt(nodes.countInput.val());
-
-                    if (nodes.countInput.val() == '' || current < 2) {
-                        nodes.countInput.val('1');
-                    }
-
-                    methods.price.updateTotalPrice();
-                },
-                keydown: function(e){
-                    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-                        (e.keyCode == 65 && e.ctrlKey === true) ||
-                        (e.keyCode >= 35 && e.keyCode <= 40)) {
-                        return;
-                    }
-                    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-                        e.preventDefault();
-                    }
-                },
-                updateTotalPrice: function(){
-                    var actual = parseInt(nodes.priceActual.val());
-
-                    nodes.totalValue.text(actual * nodes.countInput.val());
-                    nodes.counterValue.text(nodes.countInput.val());
-                    nodes.cardTotal.show();
-                }
-            },
-            recom: function(){
-                nodes.body.find('.recom').removeClass('active');
-                $(this).addClass('active');
-            },
             size: function(){
                 nodes.body.find('.size-item').removeClass('selected');
                 $(this).addClass('selected');
@@ -65,40 +12,29 @@ var card = (function(){
                 nodes.body.find('.color-item').removeClass('selected');
                 $(this).addClass('selected');
             },
-            complex: function(){
-                nodes.body.find('.card-complex-item').removeClass('selected');
-                $(this).addClass('selected');
+            rate: function(){
+                nodes.rateit.rateit();
+            },
+            recommend: function(){
+                nodes.body.find('.recom').removeClass('active');
+                $(this).addClass('active');
             },
             favorite: function(){
                 nodes.body.find('.fav-list-item').removeClass('selected');
                 $(this).addClass('selected');
             },
-            rate: function(){
-                nodes.rateit.rateit();
-            },
             events: {
                 set: function(){
                     nodes.body
-                        //.on('click', '.count-inc', methods.price.increment)
-                        //.on('click', '.count-dec', methods.price.decrement)
-                        //.on('change', '.card-count-input', methods.price.change)
-                        //.on('keydown', '.card-count-input', methods.price.keydown)
-                        .on('click','.recom', methods.recom)
                         .on('click','.size-item', methods.size)
                         .on('click','.color-item', methods.color)
-                        .on('click','.card-complex-item', methods.complex)
+                        .on('click','.recom', methods.recommend)
                         .on('click','.fav-list-item', methods.favorite);
                 }
             }
         };
     return {
         init: function(){
-            nodes.priceActual = nodes.body.find('.prod-price-actual');
-
-            if(nodes.priceActual.length != 0) {
-                //methods.price.prepare();
-            }
-
             nodes.rateit = nodes.body.find('.rateit');
 
             if(nodes.rateit.length != 0) {
--- static/js/includes/price.js
+++ static/js/includes/price.js
@@ -9,13 +9,18 @@ var price = (function(){
                 vars.num = [];
                 vars.one = [];
                 vars.total = [];
+                vars.promo = 0;
+                vars.bonus = 0;
 
                 nodes.one = nodes.price.find('.s-price-one');
                 nodes.num = nodes.price.find('.s-price-num');
                 nodes.total = nodes.price.find('.s-price-total');
+
                 nodes.input = nodes.price.find('.s-price-input');
                 nodes.dec = nodes.price.find('.s-price-dec');
                 nodes.inc = nodes.price.find('.s-price-inc');
+
+                nodes.choice = nodes.price.find('.s-price-choice');
                 nodes.select = nodes.price.find('.s-price-select');
 
                 nodes.totalSum = nodes.body.find('.s-price-total-sum');
@@ -32,8 +37,25 @@ var price = (function(){
                 });
 
                 $.each(nodes.total, function(){
-                    vars.total.push(parseInt($(this).text()));
+                    var item = $(this),
+                        chart = true;
+
+                    if(item.parent().hasClass('s-price-block')) {
+                        chart = false;
+                    }
+                    vars.total.push({
+                        value: parseInt(item.text()),
+                        chart: chart
+                    });
                 });
+
+                if(nodes.choice.length != 0) {
+                    methods.events.choice();
+                }
+
+                if(nodes.select.length != 0) {
+                    methods.events.select();
+                }
 
                 if(nodes.promo.length != 0) {
                     vars.promo = parseInt(nodes.promo.text());
@@ -44,6 +66,8 @@ var price = (function(){
                     vars.bonusMax = parseInt(nodes.body.find('.s-price-bonus-max').text());
                     nodes.bonusInput = nodes.body.find('.s-price-bonus-input');
                     nodes.bonusButton = nodes.body.find('.s-price-bonus-button');
+
+                    methods.events.bonus();
                 }
             },
             counter: {
@@ -72,6 +96,21 @@ var price = (function(){
                     }
 
                     vars.num[index] = value;
+
+                    methods.update(index);
+                }
+            },
+            choice: {
+                click: function(){
+                    var item = $(this),
+                        index = nodes.price.index(item.parents('.s-price')),
+                        value = parseInt(item.find('.card-complex-price').text());
+
+                    vars.one[index] = value;
+                    nodes.one.eq(index).text(value);
+
+                    nodes.body.find('.s-price-choice').removeClass('selected');
+                    item.addClass('selected');
 
                     methods.update(index);
                 }
@@ -112,22 +151,33 @@ var price = (function(){
             },
             update: function(index){
                 var value = vars.num[index],
-                    price = vars.one[index] * value;
+                    price = vars.one[index] * value,
+                    block =  nodes.price.eq(index).find('.s-price-block');
 
-                vars.total[index] = price;
+                vars.total[index].value = price;
 
                 nodes.input.eq(index).val(value);
                 nodes.num.eq(index).text(value);
                 nodes.total.eq(index).text(price);
 
+
+                if(block.length != 0 && block.is(':hidden')) {
+                    block.fadeIn();
+                }
+
                 if(nodes.final.length != 0) {
                     var totalSum = 0;
 
                     $.each(vars.total, function(){
-                        totalSum += this;
+                        if(this.chart) {
+                            totalSum += this.value;
+                        }
                     });
 
-                    nodes.totalSum.text(totalSum);
+                    if(nodes.totalSum.length != 0) {
+                        nodes.totalSum.text(totalSum);
+                    }
+
                     nodes.final.text(totalSum - vars.promo - vars.bonus);
                 }
             },
@@ -136,7 +186,14 @@ var price = (function(){
                     nodes.dec.on('click', methods.counter.decrement);
                     nodes.inc.on('click', methods.counter.increment);
                     nodes.input.on('change', methods.counter.change);
+                },
+                choice: function(){
+                    nodes.choice.on('click', methods.choice.click);
+                },
+                select: function(){
                     nodes.select.on('change', methods.select.change);
+                },
+                bonus: function(){
                     nodes.bonusInput.on('change', methods.bonus.change);
                     nodes.bonusButton.on('click', methods.bonus.update);
                 }
